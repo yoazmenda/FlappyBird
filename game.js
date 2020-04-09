eb6161bc2fab4485d5ab8e808ab4d7b004ced777
Yoaz Menda <yoazmenda@gmail.com>
2020-04-09 15:50:29 +0300

    first commit

---
+++ game.js
@@ -0,0 +1,164 @@
+var player;
+var myObstacles = [];
+var myScore;
+var leftKey = 37,
+    upKey = 38,
+    rightKey = 39,
+    downKey = 40;
+var pressedKeys = { "left": false, "right": false, "up": false, "down": false };
+document.addEventListener("keydown", function(e) {
+    switch (e.keyCode) {
+        case leftKey:
+            pressedKeys["left"] = true;
+            break;
+        case upKey:
+            pressedKeys["up"] = true;
+            break;
+        case rightKey:
+            pressedKeys["right"] = true;
+            break;
+        case downKey:
+            pressedKeys["down"] = true;
+            break;
+    }
+});
+document.addEventListener("keyup", function(e) {
+    switch (e.keyCode) {
+        case leftKey:
+            pressedKeys["left"] = false;
+            break;
+        case upKey:
+            pressedKeys["up"] = false;
+            break;
+        case rightKey:
+            pressedKeys["right"] = false;
+            break;
+        case downKey:
+            pressedKeys["down"] = false;
+            break;
+    }
+});
+
+function startGame() {
+    player = new component(30, 30, "red", 10, 120);
+    player.gravity = 0.05;
+    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d", );
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    this.score = 0;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.gravity = 0;
+    this.gravitySpeed = 0;
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
+    }
+    this.hitBottom = function() {
+        var rockbottom = myGameArea.canvas.height - this.height;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+            this.gravitySpeed = 0;
+        }
+    }
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
+}
+
+function updateGameArea() {
+    debug();
+    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (player.crashWith(myObstacles[i])) {
+            return;
+        }
+    }
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(150)) {
+        x = myGameArea.canvas.width;
+        minHeight = 20;
+        maxHeight = 200;
+        height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+        myObstacles.push(new component(10, height, "green", x, 0));
+        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
+    }
+    for (i = 0; i < myObstacles.length; i += 1) {
+        myObstacles[i].x += -1;
+        myObstacles[i].update();
+    }
+    myScore.text = "SCORE: " + myGameArea.frameNo;
+    myScore.update();
+    player.newPos();
+    player.update();
+}
+
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) { return true; }
+    return false;
+}
+
+function accelerate(n) {
+    player.gravity = n;
+}
+
+
+function debug() {
+    var debugText = "";
+    for (var key in pressedKeys) {
+        if (pressedKeys[key]) debugText = debugText + key + "<br>";
+    }
+    document.getElementById("pressedKeys").innerHTML = "<div>" + debugText + "</div>";
+
+}
